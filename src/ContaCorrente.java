e49dbf585ed56a20d35ade6b04780b69bfa3df3d
MarcosOliveir4 <marcosassis071@gmail.com>
2022-02-03 15:03:20 -0300

    :sparkles: novo projeto para estudo

---
+++ src/ContaCorrente.java
@@ -0,0 +1,6 @@
+public class ContaCorrente extends Conta {
+
+    public ContaCorrente(int agencia, int numero, double saldo, Cliente titular) {
+        super(agencia, numero, saldo, titular);
+    }
+}
